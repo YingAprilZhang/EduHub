0f9fcce587a39d05bf3b155cc7216599d9ff0663
ChangXuTina <xu.chang2@northeastern.edu>
2021-08-08 02:21:39 -0700

    Create class Role,Country,Request and other Chang's classes

---
+++ src/main/java/model/Role/AccountantRole.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.Role;
+
+import javax.swing.JPanel;
+import model.UserAccount.UserAccount;
+
+/**
+ *
+ * @author changxu
+ */
+public class AccountantRole extends Role{
+
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business) {
+        return new AccountantAreaJPanel(userProcessContainer, account, business);
+    }
+    
+}
