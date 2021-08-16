6935f1a2b9dd7f918f6f909924e51fe47fd3828d
Ying Zhang <zhang.ying11@northeastern.edu>
2021-08-16 16:16:01 -0700

    add EduDataAnalyze

---
+++ src/main/java/model/DataAnalyze/EduDataAnalyze.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.DataAnalyze;
+
+import model.Country.Country;
+
+/**
+ *
+ * @author aprilyz
+ */
+public class EduDataAnalyze {
+    
+    public static double getAvgMathScoreMale (Country country) {
+        return 0.0;
+    }
+    
+}
